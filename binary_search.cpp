3fbbaf5ccf9c66729e30fb3f0faa717b7768692f
Alexey <appplemac@gmail.com>
2013-02-08 17:38:08 +0100

    Erased unnecessary whitespace

--- binary_search.cpp
+++ binary_search.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 // Pre: (0 <= left) and (right < v.size()) and (v is sorted in an ascending order)
 // Post: returns a number i, left<=i<=right and v[i]==x.
-//		 If there is no element x in v[], returns -1.
+// If there is no element x in v[], returns -1.
 int position(double x, const vector<double>& v, int left, int right) {
     if (left > right) return -1;
     int pos = (left + right)/2; // the middle of v[left,right]
@@ -14,15 +14,15 @@ int position(double x, const vector<double>& v, int left, int right) {
 }
 
 int main() {
-	int size;
-	cin >> size;
-	vector<double> v(size);
-	for (int i = 0; i < size; ++i) {
-		cin >> v[i];
-	}
-	double x;
-	cin >> x;
-	int left, right;
-	cin >> left >> right;
-	cout << position(x, v, left, right);
+    int size;
+    cin >> size;
+    vector<double> v(size);
+    for (int i = 0; i < size; ++i) {
+        cin >> v[i];
+    }
+    double x;
+    cin >> x;
+    int left, right;
+    cin >> left >> right;
+    cout << position(x, v, left, right);
 }
